5bd98eaf41a35b9cb06a467df955e62800d215e8
MehediHasan128 <mehedihasan12926@gmail.com>
2023-10-19 04:02:54 +0530

    get singel data using _id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors());
@@ -31,10 +31,16 @@ async function run() {
     const productCollection = client.db('shopDB').collection('products');
 
 
+    app.get('/product/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await productCollection.findOne(query);
+        res.send(result);
+    })
+
     app.get('/products/:name', async(req, res) =>{
         const productBrand = req.params.name;
         const query = {brandName: productBrand}
-        console.log(query);
         const result = await productCollection.find(query).toArray();
         res.send(result)
     })
